251bdeae164e6c0e8e8c97a598e8309fd8747848
chhoundiya-harshika <chhoundiyaharshika@gmail.com>
2026-09-11 15:40:35 +0530

    LOGIC

---
+++ day.py
@@ -0,0 +1,17 @@
+day = int(input("Enter a number range from 1 to 6"))
+if (day==1):
+    print("Munday")
+elif(day==2):
+    print("Tuesday")
+elif(day==3):
+    print("Wednesday")
+elif(day==4):
+    print("Thurday")
+elif(day==5):
+    print("Friday")
+elif(day==6):
+    print("Saturday")
+elif(day==7):
+    print("Sunday")
+else:
+    print("Enter a valid day")
